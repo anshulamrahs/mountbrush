dee4866e47fca464e6c97f836728a3b1c8c35d57
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-05-29 12:44:13 +0530

    booking bar

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './home.scss'
 import bg from '../assets/bghero.jpeg'
 import logo from '../assets/mblogobg.png'
-import Men from '../assets/men.jpeg'
+import Men from '../assets/mens.jpeg'
 
 
 export const Home = () => {
@@ -27,8 +27,34 @@ export const Home = () => {
       </div>
     </header>
       <div className='homes'>
-        <h1>Mountbrush</h1>
-        <div className="glitch" data-glitch="Hosting you soon"></div>
+        <div className="booking">
+          <h3>Where are you going?</h3>
+          <input type="text" value="Chamba,Himachal Pradesh."></input>
+        </div>
+        <div className="checkIn">
+          <h3>Check-In</h3>
+          <input type="date" value="2023-06-01" min="2023-06-01" max="2026-12-31"></input>
+        </div>
+        <div className="checkOut">
+        <h3>Check-Out</h3>
+        <input type="date" value="2023-06-01" min="2023-06-01" max="2026-12-31"></input>
+        </div>
+        <div className="rooms">
+          <h3>Rooms?</h3>
+          <input type= "number" value="1"></input>
+        </div>
+        <div className="searchb">
+        <button className='search'>Search</button>
+        </div>
+      <div className="explore">
+        <div className="text">
+      <h2>It's Vacation time!</h2>
+       <p>Plan your Vacation With Mountbrush Tours, create memorable moments, and find everything that you have been looking for…</p>
+        </div>
+      <div className="bt">
+        <button className='disc'>Discover</button>
+      </div>
+      </div>
       </div>
     </section>
   )
