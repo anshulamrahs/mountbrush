42866883ea645c2eee1dc13fe0ca350e14d47ddd
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-01 18:02:33 +0530

    carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import Gallery from './components/gallery/Gallery';
 import Home  from './components/home/Home';
+
 
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      <Gallery/>
     </div>
   );
 }
