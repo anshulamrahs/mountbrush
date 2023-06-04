a5258b7f9ed2eb10ced7af05ff14a67f153a54ba
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-04 21:17:32 +0530

    carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -6,7 +6,10 @@ const Carousel = () => {
     <section>
         <div>Carousel
             <div className="cv">
-                <div className="crv"></div>
+                <div className="crv">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, animi hic? Quas deleniti autem nulla illo iusto aut, unde, animi tempore repudiandae odio, quidem quod vitae tenetur? Ea, reprehenderit totam.</div>
+            </div>
+            <div className="rou">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, tenetur!
             </div>
         </div>
     </section>
