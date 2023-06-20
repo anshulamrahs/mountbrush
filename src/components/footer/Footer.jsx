d7ea606867e922aad9807dbe3e3159368650c832
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-20 13:38:21 +0530

    foot

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,14 +3,8 @@ import './footer.scss'
 
 const Footer = () => {
   return (
-    <div>
-      <div className="map">Map components</div>
-      <div className="rows"></div>
-      <div className="contact">contact </div>
-      <div className="rev">
-        crew
-        <div className="crow"></div>
-      </div>
+    <div className="fr">
+      <div className="vre"></div>
     </div>
   )
 }
