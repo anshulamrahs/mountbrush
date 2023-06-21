6d5cbee621f059de711d098d154c70b9dd85b9d3
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-21 20:01:41 +0530

    origin commits

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -12,6 +12,38 @@ const Carousel = () => {
                 <ul>creator summit</ul>
                 <ul>creator summit</ul>
               </li>
+              <div className="frew">
+                <div className="crew">
+                  <nav>
+                    <header>
+                      <p>Lorem ipsum dolor, sit amet consectetur
+                         adipisicing elit. Debitis deserunt et obcaecati a 
+                         expedita consequuntur vero numquam, nulla reiciendis 
+                         possimus fugiat, praesentium maxime commodi repudiandae 
+                         repellendus eligendi reprehenderit. Unde, reprehenderit.</p>
+                    </header>
+                  </nav>
+                  <Carousel>
+                    <html lang="en">
+                    <head>
+                      <meta charset="UTF-8" />
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                      <title>Document</title>
+                    </head>
+                    <body>
+                      <div className="footer">
+                        <li>
+                          <ul><a href='#home'>Jome</a></ul>
+                          <ol>
+                            <li><a href='#contact'>Contact us</a></li>
+                          </ol>
+                        </li>
+                      </div>
+                    </body>
+                    </html>
+                  </Carousel>
+                </div>
+              </div>
             </div>
             <Section>
               <nav>
