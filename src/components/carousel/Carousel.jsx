c99f2b13759e35237d225935f9e292fc71c36ebc
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-21 10:41:51 +0530

    fcarout

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -6,6 +6,21 @@ const Carousel = () => {
     <section>
         <div className="crew">
             <div className="img"></div>
+            <div className="rew">
+              <li>
+                <ul>creator summit</ul>
+                <ul>creator summit</ul>
+                <ul>creator summit</ul>
+              </li>
+            </div>
+            <Section>
+              <nav>
+                <a href="#home" className="">Home</a>
+                <a href="#home" className="">Contact const first = useContext(second)</a>
+                <a href="#home" className="">We  from front</a>
+                <a href="#home" className="">Home</a>
+              </nav>
+            </Section>
         </div>
     </section>
   )
