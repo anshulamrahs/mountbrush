c40f5c1e87a98df5624193997a754d6ccfbbefc9
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-22 12:17:33 +0530

    origin commits

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,18 @@ import './footer.scss'
 const Footer = () => {
   return (
     <div className="fr">
-      <div className="vre"></div>
+      <div className="vre">
+        <div className="cry">
+          <div className="ren">
+            <ul>
+              <li><a href='#home'>Home</a></li>
+              <li><a href='#home'>Home</a></li>
+              <li><a href='#home'>Home</a></li>
+              <li><a href='#home'>Home</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
