c81bffec38b4b290b1b5b1f9dd0b1c9e2fabb78b
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-22 12:46:06 +0530

    origin commits

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,58 +3,11 @@ import './carousel.scss'
 
 const Carousel = () => {
   return (
-    <section>
-        <div className="crew">
-            <div className="img"></div>
-            <div className="rew">
-              <li>
-                <ul>creator summit</ul>
-                <ul>creator summit</ul>
-                <ul>creator summit</ul>
-              </li>
-              <div className="frew">
-                <div className="crew">
-                  <nav>
-                    <header>
-                      <p>Lorem ipsum dolor, sit amet consectetur
-                         adipisicing elit. Debitis deserunt et obcaecati a 
-                         expedita consequuntur vero numquam, nulla reiciendis 
-                         possimus fugiat, praesentium maxime commodi repudiandae 
-                         repellendus eligendi reprehenderit. Unde, reprehenderit.</p>
-                    </header>
-                  </nav>
-                  <Carousel>
-                    <html lang="en">
-                    <head>
-                      <meta charset="UTF-8" />
-                      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                      <title>Document</title>
-                    </head>
-                    <body>
-                      <div className="footer">
-                        <li>
-                          <ul><a href='#home'>Jome</a></ul>
-                          <ol>
-                            <li><a href='#contact'>Contact us</a></li>
-                          </ol>
-                        </li>
-                      </div>
-                    </body>
-                    </html>
-                  </Carousel>
-                </div>
-              </div>
-            </div>
-            <Section>
-              <nav>
-                <a href="#home" className="">Home</a>
-                <a href="#home" className="">Contact const first = useContext(second)</a>
-                <a href="#home" className="">We  from front</a>
-                <a href="#home" className="">Home</a>
-              </nav>
-            </Section>
-        </div>
-    </section>
+   <div className="section">
+    <div className="rew">
+      
+    </div>
+   </div>
   )
 }
 
