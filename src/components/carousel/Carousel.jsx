232deb7d8ed5dd0401c6dffdeface180eb0bdc81
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-23 20:01:47 +0530

    rev

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -5,7 +5,20 @@ const Carousel = () => {
   return (
    <div className="section">
     <div className="rew">
-      
+      <div className="jobportal">
+        <ul>
+          <li>search suitable jobs</li>
+          <div className="li">
+            apply for suitable jobs
+            <div className="li">
+              start interview prep beforhand
+            </div>
+            <div className="strat">
+              react and js all functions dhould be onhamd for sure
+            </div>
+          </div>
+        </ul>
+      </div>
     </div>
    </div>
   )
