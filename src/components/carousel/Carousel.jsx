4bf38290ca8b6c9e2ce3e523f61b1ddd258366c9
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-26 18:44:47 +0530

    round

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -4,15 +4,6 @@ import './carousel.scss'
 const Carousel = () => {
   return (
    <div className="section">
-   <div className="rew">
-    <div className="xel">
-      <il>
-        <li>
-          <a>Home</a>
-        </li>
-      </il>
-    </div>
-   </div>
    </div>
   )
 }
