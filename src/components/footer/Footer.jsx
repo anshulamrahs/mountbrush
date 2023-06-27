600c6179068877c0a6cd595537def0447828e24b
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-27 23:11:12 +0530

    round

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,14 +6,13 @@ const Footer = () => {
     <div className="fr">
       <div className="vre">
         <div className="cry">
-          <div className="ren">
-            <ul>
-              <li><a href='#home'>Home</a></li>
-              <li><a href='#home'>Home</a></li>
-              <li><a href='#home'>Home</a></li>
-              <li><a href='#home'>Home</a></li>
-            </ul>
+         <div className="vow">
+          <div className="ken">
+            <div className="ul">
+              <ul></ul>
+            </div>
           </div>
+         </div>
         </div>
       </div>
     </div>
