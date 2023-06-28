f564271ef9de62197bde0359ddeb27c876c667a9
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-06-28 17:05:01 +0530

    round

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,17 +4,7 @@ import './footer.scss'
 const Footer = () => {
   return (
     <div className="fr">
-      <div className="vre">
-        <div className="cry">
-         <div className="vow">
-          <div className="ken">
-            <div className="ul">
-              <ul></ul>
-            </div>
-          </div>
-         </div>
-        </div>
-      </div>
+     <div className="gf"></div>
     </div>
   )
 }
